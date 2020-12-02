72340fab9d1557c609284e65002be4ebe1d32eb0
replacem3nts <donny.landis@gmail.com>
2020-12-02 18:38:05 -0500

    completes section 10 overview of mongo CRUD actions

--- mongodb.js
+++ mongodb.js
@@ -15,46 +15,5 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName);
-
-    // db.collection('users').findOne(
-    //   { _id: new ObjectID('5fc6c20ba2d0dc1e86ff29ec') },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log('Unable to fetch');
-    //     }
-
-    //     console.log(user);
-    //   }
-    // );
-
-    // db.collection('users')
-    //   .find({ age: 752 })
-    //   .toArray((error, users) => {
-    //     console.log(users);
-    //   });
-
-    // db.collection('users')
-    //   .find({ age: 752 })
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
-
-    db.collection('tasks').findOne(
-      {
-        _id: new ObjectID('5fc6c46d8039681f296f016a'),
-      },
-      (error, task) => {
-        if (error) {
-          console.log('Unable to fetch');
-        }
-        console.log(task);
-      }
-    );
-
-    db.collection('tasks')
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks);
-      });
   }
 );
