217ff6cf7d903cbc8e230640d9932ee0744879b7
replacem3nts <donny.landis@gmail.com>
2020-12-02 15:00:31 -0500

    completes inserting docs, using object id, and querying docs

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 // CRUD operations
 
 const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = mongodb;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -16,9 +16,45 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Bojangles',
-      age: 752,
-    });
+    // db.collection('users').findOne(
+    //   { _id: new ObjectID('5fc6c20ba2d0dc1e86ff29ec') },
+    //   (error, user) => {
+    //     if (error) {
+    //       return console.log('Unable to fetch');
+    //     }
+
+    //     console.log(user);
+    //   }
+    // );
+
+    // db.collection('users')
+    //   .find({ age: 752 })
+    //   .toArray((error, users) => {
+    //     console.log(users);
+    //   });
+
+    // db.collection('users')
+    //   .find({ age: 752 })
+    //   .count((error, count) => {
+    //     console.log(count);
+    //   });
+
+    db.collection('tasks').findOne(
+      {
+        _id: new ObjectID('5fc6c46d8039681f296f016a'),
+      },
+      (error, task) => {
+        if (error) {
+          console.log('Unable to fetch');
+        }
+        console.log(task);
+      }
+    );
+
+    db.collection('tasks')
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        console.log(tasks);
+      });
   }
 );
